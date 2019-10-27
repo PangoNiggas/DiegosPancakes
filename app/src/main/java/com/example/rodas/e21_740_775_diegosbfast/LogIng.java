b7a9c4514d485dce771c7881b961447934f92e4c
PangoNiggas <animasdelmundo2@gmail.com>
2019-10-27 15:21:03 -0600

    first commit

---
+++ app/src/main/java/com/example/rodas/e21_740_775_diegosbfast/LogIng.java
@@ -0,0 +1,13 @@
+package com.example.rodas.e21_740_775_diegosbfast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogIng extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_ing);
+    }
+}
